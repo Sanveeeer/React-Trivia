86d47c687757b99c62453c434f4611eff0a19973
San <45203157+Sanveeeer@users.noreply.github.com>
2022-08-23 18:56:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>&lt;this.Quiz&gt;</header>
+      <h1>Frontend Trivia Quiz</h1>
+      <h2>Developed by S.Singh & E.Kos</h2>
+      <h3>Music by ...</h3>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Api Documentation: <a href="url">https://opentdb.com/api_config.php</a></p>
+  </div>
+);
+
+export default Footer;
