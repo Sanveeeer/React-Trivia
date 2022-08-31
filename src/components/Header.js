2c09613dda250f6f580b62f1da415259ab8ead68
ElaK <103433676+Ela-Kos@users.noreply.github.com>
2022-08-31 18:38:19 +0100

    #6, 7, 8 added <br> to text
    Hello again. Not sure if this will work but have added breaks into the text above to create tidy line breaks. And ever so slightly tweaked the text. Enjoy!

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import './Header.css';
   const Header = () => (
     <div className="header">
         <header>(this.quiz)</header>
-        <h1>Pit your wits against our frontend web dev 
-            quiz. Master and see if you’re good enough
+        <h1>Pit your wits against our frontend web dev.<br>
+            quiz master and see if you’re good enough<br>
             to achieve a top score!
         </h1>        
     </div>
